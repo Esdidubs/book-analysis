933f1b4e22264c81bde2caf192d2996799264b0a
Esdidubs <sward3644@gmail.com>
2020-05-24 13:28:17 -0700

    added a few more charts

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let fiveStar = d3.max(bookData, (d) => d.myRating);
 let fiveStarData = bookData.filter((d) => d.myRating === fiveStar);
 
+let outlierData = bookData.filter((d) => d.pubDate > 1970);
+
 // Items to run in the beginning of the page load
 $(function() {
 	histograms();
 	pageAverage(bookData);
 	myRatingAverage(bookData);
 	totalRatingAverage(bookData);
+	yearAverage(outlierData);
 });
 
 function histograms() {
@@ -159,7 +162,7 @@ function histograms() {
 	let colorScale = d3.scaleLinear().domain(d3.extent(bookData, (d) => d.myRating)).range([ 'pink', 'maroon' ]);
 	let xAxis4 = d3.axisBottom(xScale4).tickSize(-width + 2 * padding).tickSizeOuter(0);
 	let yAxis4 = d3.axisLeft(yScale4).tickSize(-height + 2 * padding).tickSizeOuter(0);
-	let tooltip = d3.select('body').append('div').classed('tooltip', true);
+	// let tooltip = d3.select('body').append('div').classed('tooltip', true);
 
 	d3.select('#pageRating').append('g').attr('transform', 'translate(0, ' + (height - padding) + ')').call(xAxis4);
 	d3.select('#pageRating').append('g').attr('transform', 'translate(' + padding + ', 0)').call(yAxis4);
@@ -175,10 +178,11 @@ function histograms() {
 		.attr('cx', (d) => xScale4(d.pages))
 		.attr('cy', (d) => yScale4(d.avgRating))
 		.attr('fill', (d) => colorScale(d.myRating))
-		/*
+		.attr('r', (d) => 5);
+	/*
 			.attr('r', (d) => radiusScale(d.births))
-			*/
-		.attr('r', (d) => 5)
+			
+		
 		.on('mousemove', function(d) {
 			tooltip.style('opacity', 1).style('left', d3.event.x - 85 + 'px').style('top', d3.event.x + 'px').html(`
 			  <p>Title: ${d.title}</p>
@@ -189,7 +193,7 @@ function histograms() {
 		})
 		.on('mouseout', function() {
 			tooltip.style('opacity', 0);
-		});
+		}); */
 
 	d3
 		.select('#pageRating')
@@ -222,6 +226,126 @@ function histograms() {
 		.attr('dy', '-1.1em')
 		.style('text-anchor', 'middle')
 		.text('Goodreads Rating');
+
+	let xScale5 = d3.scaleLinear().domain(d3.extent(outlierData, (d) => d.pubDate)).range([ padding, width - padding ]);
+	let yScale5 = d3
+		.scaleLinear()
+		.domain(d3.extent(outlierData, (d) => d.avgRating))
+		.range([ height - padding, padding ]);
+	let colorScale2 = d3.scaleLinear().domain(d3.extent(outlierData, (d) => d.myRating)).range([ 'pink', 'maroon' ]);
+	let xAxis5 = d3.axisBottom(xScale5).tickSize(-width + 2 * padding).tickSizeOuter(0);
+	let yAxis5 = d3.axisLeft(yScale5).tickSize(-height + 2 * padding).tickSizeOuter(0);
+	//let tooltip2 = d3.select('body').append('div').classed('tooltip', true);
+
+	d3.select('#yearRating').append('g').attr('transform', 'translate(0, ' + (height - padding) + ')').call(xAxis5);
+	d3.select('#yearRating').append('g').attr('transform', 'translate(' + padding + ', 0)').call(yAxis5);
+
+	d3
+		.select('#yearRating')
+		.attr('width', width)
+		.attr('height', height)
+		.selectAll('circle')
+		.data(outlierData)
+		.enter()
+		.append('circle')
+		.attr('cx', (d) => xScale5(d.pubDate))
+		.attr('cy', (d) => yScale5(d.avgRating))
+		.attr('fill', (d) => colorScale2(d.myRating))
+		.attr('r', (d) => 5);
+	/*
+			.attr('r', (d) => radiusScale(d.births))
+			
+		
+		.on('mousemove', function(d) {
+			tooltip2.style('opacity', 1).style('left', d3.event.x - 85 + 'px').style('top', d3.event.x + 'px').html(`
+			  <p>Title: ${d.title}</p>
+			  <p>Pages: ${d.pages}</p>
+			  <p>My Rating: ${d.myRating}</p>
+			  <p>Goodreads Rating: ${d.avgRating}</p>
+			`);
+		})
+		.on('mouseout', function() {
+			tooltip2.style('opacity', 0);
+		});*/
+
+	d3
+		.select('#yearRating')
+		.append('text')
+		.attr('x', width / 2)
+		.attr('y', height - padding)
+		.attr('dy', '1.5em')
+		.style('text-anchor', 'middle')
+		.text('Pages');
+
+	/*
+
+	d3
+		.select('#pageRating')
+		.append('text')
+		.attr('x', width / 2)
+		.attr('y', 15)
+		.style('text-anchor', 'middle')
+		.style('font-size', '1em')
+		.text('Data on Pages vs Ratings');
+
+		*/
+
+	d3
+		.select('#yearRating')
+		.append('text')
+		.attr('transform', 'rotate(-90)')
+		.attr('x', -height / 2)
+		.attr('y', padding)
+		.attr('dy', '-1.1em')
+		.style('text-anchor', 'middle')
+		.text('Goodreads Rating');
+
+	let xScale6 = d3.scaleLinear().domain(d3.extent(outlierData, (d) => d.pubDate)).range([ padding, width - padding ]);
+	let histogram6 = d3.histogram().domain(xScale6.domain()).thresholds(51).value((d) => d.pubDate);
+	let bins6 = histogram6(outlierData);
+	let barWidth6 = width / bins6.length - barPadding;
+	let yScale6 = d3.scaleLinear().domain([ 0, d3.max(bins6, (d) => d.length) ]).range([ height - padding, padding ]);
+	let xAxis6 = d3.axisBottom(xScale6).tickSize(-width + 2 * padding).tickSizeOuter(0);
+	let yAxis6 = d3.axisLeft(yScale6).tickSize(-height + 2 * padding).tickSizeOuter(0);
+	d3.select('#myYearRating').append('g').attr('transform', 'translate(0, ' + (height - padding) + ')').call(xAxis6);
+	d3.select('#myYearRating').append('g').attr('transform', 'translate(' + padding + ', 0)').call(yAxis6);
+
+	console.log(bins6);
+
+	let bars6 = d3
+		.select('#myYearRating')
+		.attr('width', width)
+		.attr('height', height)
+		.selectAll('.bar')
+		.data(bins6)
+		.enter()
+		.append('g')
+		.classed('bar', true);
+
+	bars6
+		.append('rect')
+		.attr('x', (d, i) => xScale6(d.x0))
+		.attr('y', (d) => yScale6(d.length))
+		.attr('height', (d) => height - yScale6(d.length) - padding)
+		.attr('width', (d) => xScale6(d.x1) - xScale6(d.x0) - barPadding)
+		.attr('fill', '#9c27b0');
+
+	bars6
+		.append('text')
+		.attr('x', width / 2)
+		.attr('y', height - padding)
+		.attr('dy', '1.5em')
+		.style('text-anchor', 'middle')
+		.text('Publication Year');
+
+	bars6
+		.append('text')
+		.attr('transform', 'rotate(-90)')
+		.attr('x', -height / 2)
+		.attr('y', padding)
+		.attr('dy', '-1.1em')
+		.style('text-anchor', 'middle')
+		.text('Number of Books');
 }
 
 function pageAverage(arr) {
@@ -277,6 +401,25 @@ function totalRatingAverage(arr) {
 		`<div class="graph" id="avgRatingSection">
 		<h2 class="graph__title">Books Read by Average Rating</h2>
 		<p class="graph__data" id="avgTotalRating">Goodreads Average Rating: ${ratingAvg}</p>
+	  </div> `
+	);
+}
+
+function yearAverage(arr) {
+	let yearSum = 0;
+	let yearCount = arr.length;
+	let yearAvg = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		yearSum += arr[i].pubDate;
+	}
+
+	yearAvg = (yearSum / yearCount).toFixed(0);
+
+	$('#myYearRatingSection').replaceWith(
+		`<div class="graph" id="myYearRatingSection">
+		<h2 class="graph__title">Books Read by Publication Year (Past 50 Years)</h2>
+		<p class="graph__data" id="avgTotalRating">Average Year: ${yearAvg}</p>
 	  </div> `
 	);
 }
